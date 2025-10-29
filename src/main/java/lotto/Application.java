08227a3b73dfe303c5f4be31da24aea3218db165
ahj30420 <ahj30420@naver.com>
2025-10-29 17:59:20 +0900

    feat: Application 클래스 구현 - 로또 게임 전체 흐름 제어 로직 작성 - 입력, 로또 구매, 결과 계산, 출력 순서로 동작하도록 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.service.LottoService;
+import lotto.service.LottoService.Result;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = InputView.readPurchaseAmount();
+        LottoService service = new LottoService();
+        service.purchase(purchaseAmount);
+
+        List<Lotto> lottos = service.getLottos();
+        OutputView.printLottos(lottos);
+
+        Lotto winningNumbers = InputView.readWinningNumbers();
+        int bonus = InputView.readBonusNumber();
+
+        Result result = service.calculateResult(winningNumbers, bonus);
+        OutputView.printResult(result, purchaseAmount);
     }
 }
