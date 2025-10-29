507c27d9b2c8c509e0a80dd5cc377341e428d048
ahj30420 <ahj30420@naver.com>
2025-10-29 18:00:56 +0900

    feat: LottoUtils 유틸 클래스 구현 - 1~45 범위의 랜덤한 6개 번호 생성 기능 추가

---
+++ src/main/java/lotto/util/LottoUtils.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoUtils {
+    public static Lotto generateRandomLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(lottoNumbers);
+    }
+}
